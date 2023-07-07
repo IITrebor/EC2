c9a818a7c4c4133141bfffd34aa908c7607dd0ba
IITrebor <135287101+IITrebor@users.noreply.github.com>
2023-07-07 21:50:37 +0000

    funcionando

---
+++ src/main/java/com/example/demo/SinBdController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.lang.String;
+
+@Controller
+@RequestMapping(path="/") 
+public class SinBdController {
+
+    @GetMapping(path="/")
+	public @ResponseBody String home() {
+		return "SL71296240 - Robert Roque Barzola";
+	}
+
+    @GetMapping(path="/idat/codigo")
+	public @ResponseBody String codigo() {
+		return "SL71296240";
+	}
+
+    @GetMapping(path="/idat/nombre-completo")
+	public @ResponseBody String nc() {
+		return "Robert Roque Barzola";
+	}
+}
+    
